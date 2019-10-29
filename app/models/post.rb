84998e813bbd238f7ef9835b7639adb2010a3a64
Takara <takara@influitive.com>
2019-10-29 13:16:53 -0400

    add first test for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,20 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, length: { in: 3..200 }, uniqueness: true
   validates :content, presence: true, uniqueness: true
+
   has_many :comments
   has_many :posts_categories
   has_many :categories, through: :posts_categories
   has_many :votes, as: :voteable
+
+  REJECTED_CHARS = /([.@?$%&^*!])/
+  REPLACED_CHARS = /([ :\-])/
+  def calculate_slug
+    title
+      .downcase
+      .strip
+      .gsub(REPLACED_CHARS, '_')
+      .gsub(REJECTED_CHARS, '')
+      .gsub('__', '_')
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,79 @@
 require 'rails_helper'
 
 describe Post do
-  it 'test' do
-    expect(1).to eq(1)
+  describe '#calculate_slug' do
+    let(:post) { Post.new(title: title) }
+
+    context '1 when title is a single word' do
+      let(:title) { 'title' }
+
+      it "returns the title" do
+        expect(post.calculate_slug).to eq('title')
+      end
+    end
+
+    context '2 when title is uppercase' do
+      let(:title) { 'TITLE' }
+
+      it "returns as lowercase" do
+        expect(post.calculate_slug).to eq('title')
+      end
+    end
+
+    context '3 when title has empty spaces' do
+      let(:title) { 'string title' }
+
+      it "replaces spaces with _" do
+        expect(post.calculate_slug).to eq('string_title')
+      end
+    end
+
+    context '4 when title has periods' do
+      let(:title) { 'string. title' }
+
+      it "removes ." do
+        expect(post.calculate_slug).to eq('string_title')
+      end
+    end
+
+    context '5 when title has : and space next to each other' do
+      let(:title) { 'string: title' }
+
+      it "removes replaces both and shortens to 1 _" do
+        expect(post.calculate_slug).to eq('string_title')
+      end
+    end
+
+    context '6 when title has -' do
+      let(:title) { 'string-title' }
+
+      it "removes -" do
+        expect(post.calculate_slug).to eq('string_title')
+      end
+    end
+
+    context '7 when title has capitals' do
+      let(:title) { 'StrinG CaPitalIzeD' }
+
+      it "downcase the entire title" do
+        expect(post.calculate_slug).to eq('string_capitalized')
+      end
+    end
+
+    context '8 when title has weird characters' do
+      let(:title) { 'title@?$%&^*!' }
+
+      it "removes :" do
+        expect(post.calculate_slug).to eq('title')
+      end
+    end
+
+    context '8 when title has replaceable char at beginning or end' do
+      let(:title) { ' title. ' }
+
+      it "removes :" do
+        expect(post.calculate_slug).to eq('title')
+      end
+    end
   end
 end
